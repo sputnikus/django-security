e6cf89ef08f5ddc0d81ab4903ee2243d2ac83c98
Lubos Matl <matllubos@gmail.com>
2014-09-03 16:07:03 -0500

    v0.0.6 Allow configurable max stored requests

--- security/config.py
+++ security/config.py
@@ -4,4 +4,4 @@ from django.conf import settings
 DEFAULT_THROTTLING_VALIDATORS = getattr(settings, 'DEFAULT_THROTTLING_VALIDATORS', 'security.default_validators')
 THROTTLING_FAILURE_VIEW = getattr(settings, 'THROTTLING_FAILURE_VIEW', 'security.views.throttling_failure_view')
 
-MAX_LOGGED_REQUESTS = 1000
+MAX_LOGGED_REQUESTS = getattr(settings, 'MAX_LOGGED_REQUESTS', 10000)
