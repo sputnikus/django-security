c1285508ea04e60fc34a917106a2a332bfcc69a3
Lubos Matl <matllubos@gmail.com>
2015-01-20 23:16:04 +0100

    v0.0.21 Fixed migrations

--- security/migrations/0004_auto__add_field_loggedrequest_response_body.py
+++ security/migrations/0004_auto__add_field_loggedrequest_response_body.py
@@ -3,6 +3,9 @@ from south.utils import datetime_utils as datetime
 from south.db import db
 from south.v2 import SchemaMigration
 from django.db import models
+from django.conf import settings
+
+AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
 
 
 class Migration(SchemaMigration):
@@ -37,20 +40,10 @@ class Migration(SchemaMigration):
             'response_timestamp': ('django.db.models.fields.DateTimeField', [], {}),
             'status': ('django.db.models.fields.PositiveSmallIntegerField', [], {}),
             'type': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '1'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['users.User']", 'null': 'True', 'on_delete': 'models.SET_NULL', 'blank': 'True'})
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['%s']" % AUTH_USER_MODEL, 'null': 'True', 'on_delete': 'models.SET_NULL', 'blank': 'True'})
         },
-        u'users.user': {
-            'Meta': {'object_name': 'User'},
-            'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '75'}),
-            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'null': 'True', 'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'is_verified': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'null': 'True', 'blank': 'True'}),
-            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'phone': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
-            'role': ('django.db.models.fields.PositiveSmallIntegerField', [], {})
+        AUTH_USER_MODEL.lower(): {
+            'Meta': {'object_name': AUTH_USER_MODEL.rsplit('.', 1)[1]},
         }
     }
 
