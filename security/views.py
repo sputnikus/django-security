5cbe1de3475e5992b62105becafdd7014a080db9
Lubos Matl <matllubos@gmail.com>
2016-12-09 17:28:39 +0100

    Django1.10 support

--- security/views.py
+++ security/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render_to_response
-from django.template.context import RequestContext
 from django.utils.encoding import force_text
+
+from django.shortcuts import render
 
 
 def throttling_failure_view(request, exception):
-    response = render_to_response('429.html', {'description': force_text(exception)},
-                                  context_instance=RequestContext(request))
+    response = render(request '429.html', {'description': force_text(exception)})
     response.status_code = 429
     return response
